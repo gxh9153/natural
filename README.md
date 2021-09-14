fa1ee761a9c68bc909a2e132183784fc008419d0
gxh <1320546563@qq.com>
2021-09-14 21:59:05 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+natural是一款基于SpringBoot+MyBatis-Plus的快速开发脚手架，拥有完整的权限管理功能，可对接Vue前端，开箱即用。
---
+++ src/test/java/com/gxh/natural/NaturalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gxh.natural;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NaturalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
